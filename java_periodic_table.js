b1c82941087a88d2ec40c6f91174ee5ee8663888
solorioc <solorioc@kenyon.edu>
2017-04-15 13:54:19 -0400

    Still working on hover funcitonality

--- java_periodic_table.js
+++ java_periodic_table.js
@@ -24,43 +24,58 @@ function RemoveData() {
 }
 
 $(document).ready(function() {
+//$("#atom").mouseover(function() {
 
-alert('JS Works!');	
+	alert('JS Works!');	
 
-function parse(document) {
-	$(document).find("ATOM").each(function() {
+	function parse(document) {
+		$(document).find("ATOM").each(function() {
 		
-		$("#content").append(
-			'<p> Symbol: '+$(this).find('SYMBOL').text()+
-			/*
-			'<br />Name: '+$(this).find('NAME').text()+
-			'<br /> Atomic Weight: '+$(this).find('ATOMIC_WEIGHT').text()+
-			'<br /> Atomic Number'+$(this).find('ATOMIC_NUMBER').text()+
-			'<br /> Oxidation States: '+$(this).find('OXIDATION_STATES').text()+
-			'<br /> Boiling Point: '+$(this).find('BOILING_POINT').text()+
-			'<br /> Density: '+$(this).find('DENSITY').text()+
-			'<br /> Electron Configuration: '+$(this).find('ELECTRON_CONFIGURATION').text()+
-			'<br /> Electronegativity: '+$(this).find('ELECTRONNEGATIVITY').text()+
-			'<br /> Atomic Radius: '+$(this).find('ATOMIC_RADIUS').text()+
-			'<br /> Atomic Volume: '+$(this).find('ATOMIC_VOLUME').text()+
-			'<br /> Specific Heat Capacity: '+$(this).find('SPECIFIC_HEAT_CAPACITY').text()+
-			'<br /> Ionization Potential: '+$(this).find('IONIZATION_POTENTIAL').text()+
-			'<br /> Thermal Conductivity: '+$(this).find('THERMAL_CONDUCTIVITY').text()+
-			*/
-			'</p>'
-		);	
-	});
-}
+			$("#content").append(
+				'<p> Symbol: '+$(this).find('SYMBOL').text()+
+				/*
+				'<br />Name: '+$(this).find('NAME').text()+
+				'<br /> Atomic Weight: '+$(this).find('ATOMIC_WEIGHT').text()+
+				'<br /> Atomic Number'+$(this).find('ATOMIC_NUMBER').text()+
+				'<br /> Oxidation States: '+$(this).find('OXIDATION_STATES').text()+
+				'<br /> Boiling Point: '+$(this).find('BOILING_POINT').text()+
+				'<br /> Density: '+$(this).find('DENSITY').text()+
+				'<br /> Electron Configuration: '+$(this).find('ELECTRON_CONFIGURATION').text()+
+				'<br /> Electronegativity: '+$(this).find('ELECTRONNEGATIVITY').text()+
+				'<br /> Atomic Radius: '+$(this).find('ATOMIC_RADIUS').text()+
+				'<br /> Atomic Volume: '+$(this).find('ATOMIC_VOLUME').text()+
+				'<br /> Specific Heat Capacity: '+$(this).find('SPECIFIC_HEAT_CAPACITY').text()+
+				'<br /> Ionization Potential: '+$(this).find('IONIZATION_POTENTIAL').text()+
+				'<br /> Thermal Conductivity: '+$(this).find('THERMAL_CONDUCTIVITY').text()+
+				*/
+				'</p>'
+			);	
+		});
+	}
 
-$.ajax({
-     url: 'allelements.xml', // name of file you want to parse
-     dataType: "xml",
-     success: parse,
-     error: function(){alert("Error: Something went wrong :'(");}
-     });
-});
+	$.ajax({
+		 url: 'allelements.xml', // name of file you want to parse
+		 dataType: "xml",
+		 success: parse,
+		 error: function(){alert("Error: Something went wrong :'(");}
+		 });
+     
+
+}
+);
 
 /*
-window.onload = function getName() {
-	alert('JS Works!');	
-}*/
+$("#atom").hover(function() {
+        var val = $(this).html();
+        
+        
+        /*$.ajax({
+		 url: 'allelements.xml', // name of file you want to parse
+		 dataType: "xml",
+		 success: parse,
+		 error: function(){alert("Error: Something went wrong :'(");}
+     });
+     
+     alert("Hover works!");
+}
+*/
