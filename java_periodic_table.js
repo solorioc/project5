7c1486101039fe34c71f11baad5693a264a3e86b
quiiescent <wilhelmj@kenyon.edu>
2017-04-15 13:28:16 -0400

    Beginnings of onhover wooo

--- java_periodic_table.js
+++ java_periodic_table.js
@@ -14,6 +14,15 @@
     }
 }
 
+function ShowData(element) {
+	testArea.innerText = 'This is element ' + element;
+}
+
+
+function RemoveData() {
+	testArea.innerText = '';
+}
+
 $(document).ready(function() {
 
 alert('JS Works!');	
