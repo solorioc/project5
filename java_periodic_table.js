13288f2b85c9223857798d31bd4c21ae96224ab9
solorioc <solorioc@kenyon.edu>
2017-04-18 13:52:50 -0400

    Edited XML to match masses of elements in table. Added more properties to be displayed, such as heat of fusion. Added units to values.

--- java_periodic_table.js
+++ java_periodic_table.js
@@ -18,7 +18,7 @@ var XMLRoot;
 
 $(document).ready(function() {
 
-	alert('JavaScript is working!');
+	//alert('JavaScript is working!');
 
 		$.ajax({
 			 url: 'allelements.xml', // name of file you want to parse
@@ -28,30 +28,36 @@ $(document).ready(function() {
 		 });  
 		 
 	$('.atom').on("click", function() {
-		alert('Clicking works!');
+		//alert('Clicking works!');
 		var mass = $(this).find('.mass').text();
-		alert('Mass is: ' + mass);
+		//alert('Mass is: ' + mass);
 		
 		var symNode = $(XMLRoot).find("ATOMIC_WEIGHT:contains('"+mass+"')");
 		
-		alert(symNode.text());
+		//alert(symNode.text());
 		
 		var atomNode = symNode.parent();
 		
 		$("#elementName").html($(atomNode).find('NAME').text());
 		$("#symbol").html($(atomNode).find('SYMBOL').text());
-		$("#density").html($(atomNode).find('DENSITY').text());
-		$("#specHeat").html($(atomNode).find('SPECIFIC_HEAT_CAPACITY').text());
-		$("#weight").html($(atomNode).find('ATOMIC_WEIGHT').text());
+		$("#density").html($(atomNode).find('DENSITY').text()+" g/cm^3");
+		$("#specHeat").html($(atomNode).find('SPECIFIC_HEAT_CAPACITY').text()+" J/g/K");
+		$("#weight").html($(atomNode).find('ATOMIC_WEIGHT').text()+" amu");
 		$("#electronConfig").html($(atomNode).find('ELECTRON_CONFIGURATION').text());
 		$("#ionPotential").html($(atomNode).find('IONIZATION_POTENTIAL').text());
 		$("#atomicNum").html($(atomNode).find('ATOMIC_NUMBER').text());
 		$("#electroNeg").html($(atomNode).find('ELECTRONEGATIVITY').text());
-		$("#thermConductivity").html($(atomNode).find('THERMAL_CONDUCTIVITY').text());
+		$("#thermConductivity").html($(atomNode).find('THERMAL_CONDUCTIVITY').text()+" W/m/K");
 		$("#oxStates").html($(atomNode).find('OXIDATION_STATES').text());
-		$("#atomRadius").html($(atomNode).find('ATOMIC_RADIUS').text());
-		$("#boilingPoint").html($(atomNode).find('BOILING_POINT').text());
-		$("#atomicVol").html($(atomNode).find('ATOMIC_VOLUME').text());
+		$("#atomRadius").html($(atomNode).find('ATOMIC_RADIUS').text()+" &Aring");
+		$("#boilingPoint").html($(atomNode).find('BOILING_POINT').text()+" K");
+		$("#atomicVol").html($(atomNode).find('ATOMIC_VOLUME').text()+" cm^3/mole");
+		$("#meltingPoint").html($(atomNode).find('MELTING_POINT').text()+" K");
+		$("#covalentRadius").html($(atomNode).find('Covalent_RADIUS').text()+" &Aring");
+		$("#hoFusion").html($(atomNode).find('HEAT_OF_FUSION').text()+" kJ/mole");
+		$("#hoVape").html($(atomNode).find('HEAT_OF_VAPORIZATION').text()+" kJ/mole");
+		
+		
 
 	});
 });
@@ -60,24 +66,3 @@ function getXML(document) {
 		XMLRoot = document;
 		//alert('Loaded XML!');
 }
-
-/*function parse(document) {
-		$(document).find("ATOM").each(function() {
-		
-			$("#elementName").append($(this).find('NAME').text());
-			$("#symbol").append($(this).find('SYMBOL').text());	
-			$("#density").append($(this).find('DENSITY').text());
-			$("#specHeat").append($(this).find('SPECIFIC_HEAT_CAPACITY').text());
-			$("#weight").append($(this).find('ATOMIC_WEIGHT').text());
-			$("#electronConfig").append($(this).find('ELECTRON_CONFIGURATION').text());
-			$("#ionPotential").append($(this).find('IONIZATION_POTENTIAL').text());
-			$("#atomicNum").append($(this).find('ATOMIC_NUMBER').text());
-			$("#electroNeg").append($(this).find('ELECTRONEGATIVITY').text());
-			$("#thermConductivity").append($(this).find('OXIDATION_STATES').text());
-			$("#atomRadius").append($(this).find('ATOMIC_RADIUS').text());
-			$("#boilingPoint").append($(this).find('BOILING_POINT').text());
-			$("#atomicVol").append($(this).find('ATOMIC_VOLUME').text());
-
-		});
-}*/
-
